6675692b69401a1533217a831d836d8d640368ef
envomp <envomp@ttu.ee>
2020-03-29 14:24:12 +0300

    Net Group - Change backend url for Heroku

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: "https://net-group-back.herokuapp.com/api/v1",
     responseType: "json"
 });
